d01156be412d0b22485d901c83dee1c041f71b9b
JC1223 <j.cesena1a@gmail.com>
2025-04-23 13:58:13 -0500

    question and answer working, starting file log

--- learn_agents_main.py
+++ learn_agents_main.py
@@ -1,33 +1,95 @@
 import asyncio
 import os
+import sys
+import atexit
 
 import openai
-from agents import Agent, OpenAIChatCompletionsModel, Runner, set_tracing_disabled
+from agents import Agent, OpenAIChatCompletionsModel, Runner, set_tracing_disabled, TResponseInputItem
 from dotenv import load_dotenv
 
 # Disable tracing since we're not using OpenAI.com models
 set_tracing_disabled(disabled=True)
 
 # Setup the OpenAI client to use either Azure OpenAI or GitHub Models
-load_dotenv(override=True)
+# load_dotenv(override=True)
 API_HOST = os.getenv("API_HOST", "github")
 
 if API_HOST == "github":
-    client = openai.AsyncOpenAI(base_url="https://models.inference.ai.azure.com", api_key=os.environ["GITHUB_TOKEN"])
-    MODEL_NAME = os.getenv("GITHUB_MODEL", "gpt-4o")
+    client = openai.AsyncOpenAI(base_url="https://models.github.ai/inference", api_key=os.environ["GITHUB_TOKEN"])#"https://models.inference.ai.azure.com", api_key=os.environ["GITHUB_TOKEN"])
+    MODEL_NAME = "openai/gpt-4o-mini"#os.getenv("GITHUB_MODEL", "gpt-4o")
 else:
+    # print("Exit: api_host")
     exit
-
+# print("\n")
+# print("\n")
+# print("A")
 agent = Agent(
     name="LearnGPT",
     instructions="You are a general tutor.  Help the user learn a chosen topic.",
     model=OpenAIChatCompletionsModel(model = MODEL_NAME, openai_client=client)
 )
-
+# print("B")
 async def main():
-    result = await Runner.run(agent, input="Hello World")
-    print(result.final_output)
+    input_items: list[TResponseInputItem] = []
+    convLog = ["user:\n\tHello"]
+    atexit.register(logConversation, "test", convLog)
+    input_items.append({"content": "Hello", "role": "user"})
+    result = await Runner.run(agent, input_items)
+    for new_item in result.new_items:
+        agent_name = new_item.agent.name
+        # print(new_item)
+        print(f"{agent_name}: {new_item.raw_item.content[-1].text}")
+        convLog.append(f"{agent_name}:\n\t{new_item.raw_item.content[-1].text}")
+    
+    while True:
+        user_input = input("Awaiting message...\n")
+        input_items.append({"content": user_input, "role": "user"})
+        convLog.append(f"user:\n\t{user_input}")
+        result = await Runner.run(agent, input_items)
+        for new_item in result.new_items:
+            agent_name = new_item.agent.name
+            # print(new_item)
+            print(f"{agent_name}:\n\t{new_item.raw_item.content[-1].text}")
+            convLog.append(f"{agent_name}:\n\t{new_item.raw_item.content[-1].text}")
+
+        input_items = result.to_input_list()
+        print(input_items)
 
 
-if __name__ == "main":
+    # response
+    # terminate = False
+    # asyncio.run(print,"00000000000")
+    # userIn = await asyncInput("Awaiting Input...")
+    # userIn = "Hello"
+    # print("C")
+    # result = await Runner.run(agent, input="Hello how are you?")
+    # print("D")
+    # print(result.final_output)#, flush=True)
+    # print("E")
+    # print("AAAA",flush=True)
+    
+    # userIn = await asyncInput("Awaiting Input...")
+    # userIn = "Exit"
+    # terminate = False if userIn.lower() != "exit" else True
+    # new_input = result.to_input_list() + [{"role":"user", "content":userIn}]
+    # while terminate == False:
+    #     result = await Runner.run(agent, input=new_input)
+    #     print(result.final_output)
+    #     # userIn = await asyncInput("Awaiting Input...")
+    #     terminate = False if userIn.lower() != "exit" else True
+    #     new_input = result.to_input_list() + [{"role":"user", "content":userIn}]
+    #     # asyncio.run(print,"AAAAAAAA")
+    # asyncio.run(print,"BBBBBBBBBB")
+    
+# async def asyncInput(string: str) -> str:
+#     await asyncio.to_thread(sys.stdout.write, f'{string} ')
+#     return await asyncio.to_thread(sys.stdin.readLine)
+
+def logConversation(filename, conversation):
+    with open(filename+".txt","x") as file:
+        file.write(conversation)
+    
+# print("F")
+if __name__ == "__main__":
     asyncio.run(main())
+# print("G")
